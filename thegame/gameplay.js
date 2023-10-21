54db5bcff3cb5640b235f0e54404e420a91db10c
Arick Smith <ArickTheDisciple@gmail.com>
2023-10-21 16:02:29 -0600

    Nogo

--- thegame/gameplay.js
+++ thegame/gameplay.js
@@ -65,23 +65,7 @@ else if (mapNum == 2){
     ];
     AnswerPos = [[0, 40], [0, 375], [canvas.width-100, 375], [canvas.width-100 , 40]];
 }
-else if (mapNum == 3){
-    gamePlatforms = [
-        new Platform(355, 200, 25, 270),
-        new Platform(455, 175, 25, 290),
-        new Platform(555, 150, 25, 300),
-        new Platform(655, 175, 25, 290),
-        new Platform(755, 200, 25, 270),
-        new Platform(0, 0, 20, 555),
-        new Platform(0, 445, 165, 25),
-        new Platform(205, 390, 165, 25),
-        new Platform(0, 345, 160, 25),
-        new Platform(205, 290, 165, 25),
-        new Platform(0, 245, 155, 25),
-        new Platform(205, 200, 165, 25)
-    ];
-    AnswerPos = [[365, 180], [465, 150], [570, 150], [670, 180]];
-}
+
 
 
 let game1 = new Map(gamePlatforms);
