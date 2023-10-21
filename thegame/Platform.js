fe44c3edfafe9d7590151b92c39fc6fbefc6c7c9
dylan <136660038+Throarin@users.noreply.github.com>
2023-10-21 12:21:44 -0600

    i really don't know

--- thegame/Platform.js
+++ thegame/Platform.js
@@ -1,9 +1,18 @@
 class Platform {
+<<<<<<< Updated upstream
     constructor(x, y, width, height) {
+=======
+    constructor(x, y, width=100, height=10, moving=false) {
+>>>>>>> Stashed changes
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+<<<<<<< Updated upstream
+=======
+        this.color = "#ff5"
+        this.moving = moving;
+>>>>>>> Stashed changes
     }
 
     draw() {
